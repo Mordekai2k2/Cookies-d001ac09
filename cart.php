2136f8fd436183073ac6e436ac0ff3f15be06b66
Randy Brouwer <2105070@talnet.nl>
2019-12-04 09:16:37 +0100

    Uitwerking van de exercise

---
+++ cart.php
@@ -0,0 +1,22 @@
+<?php
+Setcookie ()
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Cookies :3</title>
+</head>
+<body>
+
+<h1>Skateboard #1</h1>
+<h2>Basketbal #2</h2>
+<h3>skeelers #3</h3>
+
+<form>
+    <input type="text" id="name" name="name" required
+           minlength="4" maxlength="20" width="300px">
+</form>
+
+
+</body>
+</html>
